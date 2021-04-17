a1c6aa8c8ebc3af704e5449a9b92687de8cf5507
Bhavik Gosrani <bhavik4748@gmail.com>
2021-04-17 00:21:46 -0400

    Update chatWindowforUsers component for auto-scroll to bottom

--- src/components/chat/chatWindow/chatWindowForUsers/chatWindowForUsers.js
+++ src/components/chat/chatWindow/chatWindowForUsers/chatWindowForUsers.js
@@ -1,23 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import api from '../../../../services/api';
 
 import classes from './chatWindowForUsers.module.css';
 
+const AlwaysScrollToBottom = () => {
+    const elementRef = useRef();
+    useEffect(() => elementRef.current.scrollIntoView());
+    return <div ref={elementRef} />;
+};
+
+
 export const ChatWindowForUsers = (props) => {
     const [messages, setMessages] = useState([]);
     let userChatBox = null;
-  //  console.log('aaaaa', props.roomId);
+    let userChatBox1 = null;
     useEffect(() => {
         async function getRoomMessages(id) {
             const msg = await api.getRoomMessages(id);
-         //   console.log({ msg })
             setMessages(msg);
         }
         getRoomMessages(props.roomId);
     }, [setMessages, props.roomId]);
 
 
-    if (messages.length > 0)
+    if (messages.length > 0) {
         userChatBox = messages.map(x => {
             return (<div key={x.id} className={classes.msgBox}>
                 <div className={classes.msg}>{x.message}  </div>
@@ -25,25 +31,38 @@ export const ChatWindowForUsers = (props) => {
             </div>
             )
         })
+        userChatBox1 = messages.map(x => {
+            return (<div key={x.id} className={classes.msgBox}>
+                <div className={classes.msg}>{x.message}  </div>
+                <div className={classes.msgUser}>{x.name}</div>
+            </div>
+            )
+        })
+    }
+
 
 
 
     let userTextBox = (
-        <>
+        <form>
             <div className={classes.textArea}>
                 <textarea placeholder="Type a message..."></textarea>
             </div>
             <div>
                 <button className={classes.sendBtn}>Send</button>
             </div>
-        </>
+        </form>
     )
 
 
 
     return (
         <div className={classes.chatWindow}>
-            <div className={classes.chatMessagesWindow}>{userChatBox}</div>
+            <div className={classes.chatMessagesWindow}>
+                <div className={classes.overflowAuto}>
+                    {userChatBox} {userChatBox1} <AlwaysScrollToBottom />
+                </div>
+            </div>
             <div className={classes.chatTextEnterWindow}>{userTextBox}</div>
         </div>
     )
